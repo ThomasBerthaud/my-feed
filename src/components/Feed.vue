59bf685e4cded4cb38e2dbe44dc1c737a7ab00f8
Thomas Berthaud <tomberthaud@gmail.com>
2019-09-18 21:49:38 +0200

    chore: small visual improvements

--- src/components/Feed.vue
+++ src/components/Feed.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="">
+  <div class>
     <div v-if="feed" class="card">
       <header class="card-header">
         <p class="card-header-title">{{ feed.title }}</p>
@@ -9,29 +9,32 @@
           </span>
         </router-link>
       </header>
-      <div class="card-content">
+      <div class="card-content limit-size">
         <div class="content">
           <div v-html="feed.entries[0].summary"></div>
           <time :datetime="feed.updated">{{ readableUpdated }}</time>
         </div>
       </div>
       <div class="card-footer">
-        <router-link :to="'/details/' + feed.id" class="card-footer-item">
-          Voir les autres...
-        </router-link>
+        <router-link :to="'/details/' + feed.id" class="card-footer-item">Voir les autres...</router-link>
         <a :href="feed.entries[0].link" class="card-footer-item" aria-label="external link">
           <span>Acceder au site</span>
-          <span class="icon"><i class="fas fa-external-link-alt"></i></span>
+          <span class="icon">
+            <i class="fas fa-external-link-alt"></i>
+          </span>
         </a>
       </div>
     </div>
     <div v-if="!feed" class="card">
       <header class="card-header">
-        <p class="card-header-title">{{ feedUrl }}</p>
+        <p class="card-header-title">
+          {{ feedUrl }}
+          <i v-if="loading" class="fas fa-circle-notch fa-spin"></i>
+        </p>
       </header>
       <div class="card-content">
         <div class="content">
-          Problème avec ce feed
+          <span v-if="!loading">Problème avec ce feed</span>
         </div>
       </div>
     </div>
@@ -50,6 +53,7 @@ export default {
   },
   data() {
     return {
+      loading: true,
       feed: null
     };
   },
@@ -60,9 +64,14 @@ export default {
     }
   },
   created() {
-    axios.get(`/feed/latest?url=${this.feedUrl}`).then(response => {
-      this.feed = response.data;
-    });
+    this.loading = true;
+    axios.get(`/feed/latest?url=${this.feedUrl}`).then(
+      response => {
+        this.feed = response.data;
+        this.loading = false;
+      },
+      () => (this.loading = false)
+    );
   }
 };
 </script>
@@ -70,5 +79,9 @@ export default {
 <style scoped>
 .card-header-title {
   word-break: break-all;
+}
+.limit-size {
+  max-height: 50vh;
+  overflow-y: scroll;
 }
 </style>
