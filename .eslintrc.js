66df2917471d7d8c8de675b2e1f86c22d97373bf
Thomas Berthaud <tomberthaud@gmail.com>
2019-09-18 22:16:00 +0200

    chore: added jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,12 @@
 module.exports = {
   root: true,
+
   env: {
     node: true
   },
+
   extends: ["plugin:vue/essential", "@vue/prettier"],
+
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
@@ -14,7 +17,17 @@ module.exports = {
       }
     ]
   },
+
   parserOptions: {
     parser: "babel-eslint"
-  }
+  },
+
+  overrides: [
+    {
+      files: ["**/__tests__/*.{j,t}s?(x)"],
+      env: {
+        jest: true
+      }
+    }
+  ]
 };
